488e1640529f96225a6672d65110dd7d0179e522
apsmith <apsmith@ccs.neu.edu>
2020-07-14 18:52:34 -0400

    First deploy commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const { connectToDb } = require('./db.js');
+const { installHandler } = require('./api_handler.js');
+const auth = require('./auth.js');
+
+const app = express();
+
+app.use(cookieParser());
+app.use('/auth', auth.routes);
+
+installHandler(app);
+
+const port = process.env.API_SERVER_PORT || 3000;
+
+app.use(express.static('public'));
+
+(async function start() {
+  try {
+    await connectToDb();
+    app.listen(port, () => {
+      console.log(`API Server started on port ${port}`);
+    });
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+}());
